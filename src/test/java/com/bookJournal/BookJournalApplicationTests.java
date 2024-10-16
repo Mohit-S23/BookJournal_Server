2cd33a17ee174372c8f8891029fc95c8e1b0950f
Mohit-S23 <sharma.mohit0323@gmail.com>
2024-10-16 23:30:14 +0530

    Initial commit

---
+++ src/test/java/com/bookJournal/BookJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
